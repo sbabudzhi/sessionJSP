cc7f928362b4b76dec750e7c2baf0908ce7cbbde
babudzhi <babudhi@lanit.ru>
2018-05-18 11:57:46 +0300

    аа

--- src/main/java/ru/babudzhi/Servlet1.java
+++ src/main/java/ru/babudzhi/Servlet1.java
@@ -8,9 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
+import org.h2.Driver;
+
+
 
 @WebServlet("/welcome")
 public class Servlet1 extends HttpServlet {
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
@@ -28,10 +33,38 @@ public class Servlet1 extends HttpServlet {
                     session.getAttribute("name3") + " " + session.getAttribute("name") + " " + session.getAttribute("name2") + "</p>"
             );
 
+
+            try (Connection db = DriverManager.getConnection("jdbc:h2:~/test", "sa", "")) {
+                try (Statement dataQuery = db.createStatement()) {
+                    dataQuery.execute(CREATE_QUERY);
+                    dataQuery.execute(DATA_QUERY);
+                }
+
+                try (PreparedStatement query =
+                             db.prepareStatement("SELECT * FROM TEST123")) {
+                    ResultSet rs = query.executeQuery();
+                    while (rs.next()) {
+                        System.out.println(String.format("%s, %s!",
+                                rs.getString(1),
+                                rs.getString("name1")));
+                    }
+                    rs.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println("Database connection failure: "
+                        + ex.getMessage());
+            }
+
         } catch (IOException e) {
 
             e.printStackTrace();
         }
-
     }
+        private static final String CREATE_QUERY =
+                "CREATE TABLE TEST123 (name3 VARCHAR(45), name1 VARCHAR(45), name2 varchar (45))";
+
+        private static final String DATA_QUERY =
+                "INSERT INTO TEST123 VALUES('бабуджи', 'светлана', 'юрьевна')";
+
+
 }
